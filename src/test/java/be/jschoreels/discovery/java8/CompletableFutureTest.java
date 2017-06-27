7b5637b043e5f98e07f9d9cc97d6f0d139a16449
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2017-06-27 22:10:48 +0200

    Improves CF.anyOf and CF.allOf tests

--- src/test/java/be/jschoreels/discovery/java8/CompletableFutureTest.java
+++ src/test/java/be/jschoreels/discovery/java8/CompletableFutureTest.java
@@ -17,10 +17,10 @@ import java.util.concurrent.ThreadPoolExecutor;
  */
 public class CompletableFutureTest {
 
-    private double epsilon = 10;
     private final int delay = 150;
     private final int fasterDelay = 50;
     private final int slowerDelay = 250;
+    private double epsilon = 10;
 
     @Test
     public void smokeTest() throws ExecutionException, InterruptedException {
@@ -29,26 +29,46 @@ public class CompletableFutureTest {
         Assert.assertEquals((Integer) delay, result);
     }
 
+    private Integer delay(Integer ms) {
+        try {
+            Thread.sleep(ms);
+            return ms;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Test
     public void waitLongerTest() throws ExecutionException, InterruptedException {
+        final CompletableFuture<Integer> fasterCF = CompletableFuture.supplyAsync(() -> delay(fasterDelay));
+        final CompletableFuture<Integer> slowerCF = CompletableFuture.supplyAsync(() -> delay(slowerDelay));
         CompletableFuture<Object> cf = CompletableFuture.anyOf(
-            CompletableFuture.supplyAsync(() -> delay(fasterDelay)),
-            CompletableFuture.supplyAsync(() -> delay(slowerDelay))
+            fasterCF,
+            slowerCF
         );
         final Integer result = (Integer) cf.join();
+        slowerCF.cancel(true); // Only works if CF is not completed
         Assert.assertEquals((Integer) fasterDelay, result);
+        Assert.assertTrue(fasterCF.isDone());
+        Assert.assertTrue(slowerCF.isCancelled());
     }
 
     @Test
     public void waitSlowerTest() throws ExecutionException, InterruptedException {
+        final CompletableFuture<Integer> fasterCF = CompletableFuture.supplyAsync(() -> delay(fasterDelay));
+        final CompletableFuture<Integer> slowerCF = CompletableFuture.supplyAsync(() -> delay(slowerDelay));
+
         CompletableFuture<Void> cf = CompletableFuture.allOf(
-            CompletableFuture.supplyAsync(() -> delay(fasterDelay)),
-            CompletableFuture.supplyAsync(() -> delay(slowerDelay))
+            fasterCF,
+            slowerCF
         );
+
         Instant startInstant = Instant.now();
         cf.join();
         Duration durationOfTest = Duration.between(startInstant, Instant.now());
-        Assert.assertEquals(slowerDelay, durationOfTest.toMillis()); // duration measure less precise
+        Assert.assertEquals(slowerDelay, durationOfTest.toMillis(), epsilon); // duration measure less precise
+        Assert.assertTrue(fasterCF.isDone());
+        Assert.assertTrue(slowerCF.isDone());
     }
 
     @Test
@@ -68,7 +88,7 @@ public class CompletableFutureTest {
         final Integer combinedDelay = CompletableFuture.supplyAsync(() -> delay(slowerDelay), singleThreadExecutor)
             .thenCombine(CompletableFuture.supplyAsync(() -> delay(slowerDelay), singleThreadExecutor),
                 (x, y) -> x + y)
-            .get();
+            .join();
 
         final Duration durationJoin = Duration.between(startInstant, Instant.now());
         final long durationJoinInMillis = durationJoin.toMillis();
@@ -83,9 +103,11 @@ public class CompletableFutureTest {
         final Instant startInstant = Instant.now();
         final ThreadPoolExecutor fixedThreadPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
 
-        final CompletableFuture<Integer> combinedCompletableFuture = CompletableFuture.supplyAsync(() -> delay(slowerDelay), fixedThreadPool)
-            .thenCombine(CompletableFuture.supplyAsync(() -> delay(slowerDelay), fixedThreadPool),
-                (x, y) -> x + y);
+        final CompletableFuture<Integer> combinedCompletableFuture =
+            CompletableFuture
+                .supplyAsync(() -> delay(slowerDelay), fixedThreadPool)
+                .thenCombine(CompletableFuture.supplyAsync(() -> delay(slowerDelay), fixedThreadPool),
+                    (x, y) -> x + y);
 
         final Integer joinResult = combinedCompletableFuture.join();
         final Duration durationJoin = Duration.between(startInstant, Instant.now());
@@ -99,42 +121,33 @@ public class CompletableFutureTest {
     }
 
     @Test
-    public void ignoreException(){
+    public void ignoreException() {
         CompletableFuture.supplyAsync(() -> delayedException(fasterDelay));
-        
+
         final Integer delay = delay(300);
         Assert.assertEquals(300, (int) delay); // nothing happend, good !
     }
 
-    @Test
-    public void propagateException(){
+    private Integer delayedException(Integer ms) {
         try {
-            final CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> delayedException(fasterDelay));
+            Thread.sleep(ms);
+            throw new RuntimeException(String.valueOf(ms));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void propagateException() {
+        try {
+            final CompletableFuture<Integer> completableFuture =
+                CompletableFuture.supplyAsync(() -> delayedException(fasterDelay));
 
             delay(300);
             completableFuture.join();
             Assert.fail();
         } catch (RuntimeException e) {
             Assert.assertEquals("java.lang.RuntimeException: " + String.valueOf(fasterDelay), e.getMessage());
-        }
-    }
-
-
-    private Integer delay(Integer ms){
-        try {
-            Thread.sleep(ms);
-            return ms;
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Integer delayedException(Integer ms){
-        try {
-            Thread.sleep(ms);
-            throw new RuntimeException(String.valueOf(ms));
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
     }
 
